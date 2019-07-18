97d9fc670c5f26e89dca5a8812548671b2f9d709
Matthew Mata <matthewmata1030@gmail.com>
2019-07-18 16:42:16 -0700

    [init] finished initial setup of server and client

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
+const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static(path.resolve(__dirname, '../client/dist')))
+
+
+app.listen(port, () => console.log(`Matthew's amazing Portfolio listening on port ${port}!`))
