105463d2a5ef1addbd21149e5440ae47161c07d7
Matthew Mata <matthewmata1030@gmail.com>
2019-07-18 17:53:30 -0700

    [init] finished skeleton

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const combinedLoaders = require('webpack-combine-loaders');
 
 module.exports = {
   entry: path.resolve(__dirname, "./client/src"),
@@ -16,7 +17,39 @@ module.exports = {
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"]
         }
-      }
+      },
+      {
+        test: /\.scss$/,
+        loader: combinedLoaders([
+          {
+            loader: 'style-loader'
+          }, 
+          {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              localIdentName: '[name]__[loader]__[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ])
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+          }
+        }]
+      },
     ]
   },
   resolve: {
