91118051d223f9d67dc94a872f29b712553c9171
Matthew Mata <matthewmata1030@gmail.com>
2019-07-20 17:14:25 -0700

    [feat] finished basic about implementation

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import style from './About.scss';
+import profilePic from './imgs/hedgehog.jpg'
 
 export default class About extends Component {
   constructor(props) {
@@ -10,9 +11,22 @@ export default class About extends Component {
   render() {
     return (
       <div className={style.mainContainer}>
-        <div>
-
-        </div>
+        <span className={style.row}>
+          <hr className={style.separator}/>
+          <h3 className={style.title}>
+            About Me
+          </h3>
+          <hr className={style.separator}/>
+        </span>
+        <p className={style.firstSentence}>
+          Hello! I'm Matthew, a full stack software engineer currently based in Los Angeles, CA.
+        </p>
+        <span className={style.row}>
+          <img src={profilePic} className={style.profilePic}/>
+          <p className={style.paragraph}>
+            My passion for coding really started when I realized I could build amazing applications that could impact people's lives in a positive way. When I'm not coding, I usually spend my free time either playing Ping Pong or trying to find new music in the dark depths of SoundCloud. I also love traveling. My favorite place so far is Japan.
+          </p>
+        </span>
       </div>
     )
   }
