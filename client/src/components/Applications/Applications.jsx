50e2f8431be079f1962816d37e2592756716e589
Matthew Mata <matthewmata1030@gmail.com>
2019-08-05 14:37:36 -0700

    [prog] added stylistic changes to Applications

--- client/src/components/Applications/Applications.jsx
+++ client/src/components/Applications/Applications.jsx
@@ -10,7 +10,53 @@ export default class Applications extends Component {
   render() {
     return (
       <section className={style.applicationsContainer}>
-        
+        <div>
+          <span className={style.row}>
+            <hr className={style.separator}/>
+            <h3 className={style.title}>
+              Portfolio
+            </h3>
+            <hr className={style.separator}/>
+          </span>
+        </div>
+        <div className={style.row}>
+          <img className={style.image} src="https://media3.giphy.com/media/A06UFEx8jxEwU/source.gif"></img>
+          <div className={style.projectDescriptionBox}>
+            <h2>
+              Application Title 
+            </h2>
+            <h3>
+              React | Redux | Mongo | Express
+            </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          </div>
+        </div>
+        <hr className={style.divider}/>
+        <div className={style.row}>
+          <img className={style.image} src="https://media3.giphy.com/media/A06UFEx8jxEwU/source.gif"></img>
+          <div className={style.projectDescriptionBox}>
+            <h2>
+              Application Title 
+            </h2>
+            <h3>
+              React | Redux | Mongo | Express
+            </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          </div>
+        </div>
+        <hr className={style.divider}/>
+        <div className={style.row}>
+          <img className={style.image} src="https://media3.giphy.com/media/A06UFEx8jxEwU/source.gif"></img>
+          <div className={style.projectDescriptionBox}>
+            <h2>
+              Application Title 
+            </h2>
+            <h3>
+              React | Redux | Mongo | Express
+            </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          </div>
+        </div>
       </section>
     )
   }
